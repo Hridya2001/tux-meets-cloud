808cd156342645dfe251e695cc6c6097672051e9
Hridya Umesh <hridyaumesh2@gmail.com>
2025-06-21 19:31:24 +0530

    Update cloud_commands.sh

--- cloud_commands.sh
+++ cloud_commands.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+
+# Connect to EC2 instance
+ssh -i "my-key.pem" ec2-user@
+
+# Check disk usage
+df -h
+
+# List block devices
+lsblk
+
+# Format a volume
+sudo mkfs -t xfs /dev/xvdf
+
+# Mount the volume
+sudo mkdir /mnt/data
+sudo mount /dev/xvdf /mnt/data
+
+# Make it persistent
+echo '/dev/xvdf /mnt/data xfs defaults,nofail 0 0' | sudo tee -a /etc/fstab
 
